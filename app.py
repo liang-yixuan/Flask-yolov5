028c63a012239f00b1d0d252e8246f0ed1c641f8
Yixuan Liang <yixuanliang97@gmail.com>
2021-09-02 23:47:19 +1000

    Add all models

--- app.py
+++ app.py
@@ -39,45 +39,82 @@ def get():
 
 
 # custom post request
-# version 1.0: returns only 1 object per image, can match multiple model outputs for the same object
+# version 2.0: returns multiple objects per image, can match multiple model outputs for the same object
 @app.route('/detections', methods=['POST'])
 def return_json():
     file = extract_img(request)
     img_bytes = file.read()
     response = []
 
-    emotion_result = get_prediction(img_bytes,dictOfModels['yolov5s_emotion'])
-    age_result = get_prediction(img_bytes,dictOfModels['yolov5s_age'])
+    emotion_result = get_prediction(img_bytes,dictOfModels['yolov5l_emotion'])
+    age_result = get_prediction(img_bytes,dictOfModels['yolov5l_age'])
+    ethnicity_result = get_prediction(img_bytes,dictOfModels['yolov5l_ethnicity']) ## placeholder for ethnicity model
+    gender_result = get_prediction(img_bytes,dictOfModels['yolov5l_gender']) ## placeholder for gender model
 
     emotion_predition = emotion_result.pred[0].numpy()
     age_predition = age_result.pred[0].numpy()
+    ethnicity_predition = ethnicity_result.pred[0].numpy()
+    gender_predition = gender_result.pred[0].numpy()
 
-    print(emotion_predition)
-    print(age_predition)
+    print("emotion_predition", emotion_predition)
+    print("age_predition", age_predition)
+    print("ethnicity_predition", ethnicity_predition)
+    print("gender_predition", gender_predition)
+
+
 
     # getting the baseline predictions
     for id, prediction in enumerate(emotion_predition):
-        print("emo prediction", id, "is", prediction)
         result = {
-            "object": id,
+            "object": id+1,
             "emo_box": list(map(lambda x: float("{0:.2f}".format(x)), prediction[:4])),
             "emo_class" : int(prediction[5]),
-            "emo_class_label" : emotion_result.names[int(prediction[5])],
-            "emo_confidence" : float("{0:.2f}".format(prediction[4]))
+            "emo_class_label" : emotion_result.names[int(prediction[5])].capitalize(),
+            "emo_confidence" : float("{0:.2f}".format(prediction[4])),
+            "age_class" : -1, 
+            "age_class_label" : "Could not detect",  
+            "age_confidence" : 0.0,
+            "ethnicity_class" : -1,
+            "ethnicity_class_label" : "Could not detect",  
+            "ethnicity_confidence" : 0.0,
+            "gender_class" : -1,
+            "gender_class_label" : "Could not detect",  
+            "gender_confidence" : 0.0
         }
         response.append(result)
     
     # match age model with the baseline predition
     for prediction in age_predition:
-        print("age prediction is", prediction)
         box = list(map(lambda x: float("{0:.2f}".format(x)), prediction[:4]))
         for object in response:
             diff = abs(np.array(object["emo_box"]) - np.array(box))
-            isSameObject = (diff < [10]* 4 ).all() # set error threshold
+            isSameObject = (diff < [20]* 4 ).all() # set error threshold
             if isSameObject:
                 object["age_class"] = int(prediction[5])
                 object["age_class_label"] = age_result.names[int(prediction[5])]
                 object["age_confidence"] = float("{0:.2f}".format(prediction[4]))
+
+    # match age model with the baseline predition
+    for prediction in ethnicity_predition:
+        box = list(map(lambda x: float("{0:.2f}".format(x)), prediction[:4]))
+        for object in response:
+            diff = abs(np.array(object["emo_box"]) - np.array(box))
+            isSameObject = (diff < [20]* 4 ).all() # set error threshold
+            if isSameObject:
+                object["ethnicity_class"] = int(prediction[5])
+                object["ethnicity_class_label"] = ethnicity_result.names[int(prediction[5])].capitalize()
+                object["ethnicity_confidence"] = float("{0:.2f}".format(prediction[4]))
+
+    # match age model with the baseline predition
+    for prediction in gender_predition:
+        box = list(map(lambda x: float("{0:.2f}".format(x)), prediction[:4]))
+        for object in response:
+            diff = abs(np.array(object["emo_box"]) - np.array(box))
+            isSameObject = (diff < [20]* 4 ).all() # set error threshold
+            if isSameObject:
+                object["gender_class"] = int(prediction[5])
+                object["gender_class_label"] = gender_result.names[int(prediction[5])].capitalize()
+                object["gender_confidence"] = float("{0:.2f}".format(prediction[4]))
 
     return jsonify({"response":response}), 200
 
@@ -86,35 +123,45 @@ def return_json():
 @app.route('/', methods=['POST'])
 def predict():
     file = extract_img(request)
-    img_bytes = file.read()
+    img_bytes = file.read() # 'bytes' object
+    
 
-    # choice of the model
-    # results = get_prediction(img_bytes,dictOfModels[request.form.get("model_choice")])
-    # print(f'User selected model : {request.form.get("model_choice")}')
 
-    age_result = get_prediction(img_bytes,dictOfModels['yolov5s_age'])
-    emotion_result = get_prediction(img_bytes,dictOfModels['yolov5s_emotion'])
-    results = emotion_result
+    # age_result = get_prediction(img_bytes,dictOfModels['yolov5s_age'])
+    emotion_result = get_prediction(img_bytes,dictOfModels['yolov5l_emotion'])
+    result = emotion_result
     # updates results.imgs with boxes and labels
-    results.render()
+    # results.render()
+    predition = result.pred[0].numpy()
+    objects = []
 
-    # encoding the resulting image and return it
-    for img in results.imgs:
-        RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        im_arr = cv2.imencode('.jpg', RGB_img)[1]
-        response = make_response(im_arr.tobytes())
-        response.headers['Content-Type'] = 'image/jpeg'
+    # getting the baseline predictions
+    for id, prediction in enumerate(predition):
+        coordinates = list(map(lambda x: float("{0:.2f}".format(x)), prediction[:4]))
+        object = {
+            "left" : coordinates[0],
+            "top" : coordinates[1],
+            "right" : coordinates[2],
+            "bottom" : coordinates[3],
+            "label" : "Person " + str(id+1)
+        }
+        # print("object", object)
+        objects.append(object)
+
+
+    # # encoding the resulting image and return it
+    # for img in results.imgs: # 'numpy.ndarray' object
+    
+    img = Image.open(io.BytesIO(img_bytes))
+    img_draw = drawBoundingBoxes(np.array(img), objects)
+    RGB_img = cv2.cvtColor(img_draw, cv2.COLOR_BGR2RGB)
+    im_arr = cv2.imencode('.jpg', RGB_img)[1]
+    response = make_response(im_arr.tobytes())
+    response.headers['Content-Type'] = 'image/jpeg'
+    
+
     return response
 
-# dummy post method
-@app.route('/dummy', methods=['GET', 'POST'])
-def dummy():
-    if request.method == 'POST':
-        word = request.form["greeting"]
-        print(word)
-    else:
-        word = "No sound"
-    return {"status" : "success", "message" : word}
 
 def extract_img(request):
     # checking if image uploaded is valid
@@ -137,7 +184,7 @@ def get_prediction(img_bytes,model):
     return results
 
 
-def drawBoundingBoxes(imageData, imageOutputPath, inferenceResults, color):
+def drawBoundingBoxes(imageData, inferenceResults): 
     """Draw bounding boxes on an image.
     imageData: image data in numpy array format
     imageOutputPath: output image file path
@@ -147,14 +194,15 @@ def drawBoundingBoxes(imageData, imageOutputPath, inferenceResults, color):
     for res in inferenceResults:
         left = int(res['left'])
         top = int(res['top'])
-        right = int(res['left']) + int(res['width'])
-        bottom = int(res['top']) + int(res['height'])
+        right = int(res['right']) 
+        bottom = int(res['bottom']) 
         label = res['label']
+        color = (255, 199, 50)
         imgHeight, imgWidth, _ = imageData.shape
-        thick = int((imgHeight + imgWidth) // 900)
-        print(left, top, right, bottom)
+        thick = int((imgHeight + imgWidth) // 300)
         cv2.rectangle(imageData,(left, top), (right, bottom), color, thick)
-        cv2.putText(imageData, label, (left, top - 12), 0, 1e-3 * imgHeight, color, thick//3)
+        cv2.putText(imageData, label, (left, top - 5), 2, 1e-3 * (imgHeight + imgWidth), color, thick//2)
+
     return imageData
     
 
